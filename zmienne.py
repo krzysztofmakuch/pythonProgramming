8573900e057b2c127e28f6a9bf6a5c499996edc9
krzysztofmakuch <55457951+krzysztofmakuch@users.noreply.github.com>
2020-10-07 21:44:54 +0200

    Add files via upload

---
+++ zmienne.py
@@ -0,0 +1,25 @@
+'''
+inicjalizacja zmiennych a i b. Jak widac zmiennym w pythonie nie musimy
+wprost przypisywac typu. Prosze zwrocic uwage w jaki sposob moge wstawic
+komentarze do skryptu i jak je zawijam
+'''
+#int:
+a = 1
+#float:
+b = 4.5
+
+''' teraz jakies operacje na zmiennych '''
+c = a - b       #inicjalizacja zmiennej c
+
+print(a == b)   #zwraca wartosc logiczna boolean
+print(c)        #zwraca liczbe
+
+#konwersja typów
+d = int(b)
+e = float(a)
+
+print('to jest zmienna przed konwersja: ', b, '; a to po konwersji: ', d)
+print('to jest zmienna przed konwersja: ', a, '; a to po konwersji: ', e)
+
+
+
