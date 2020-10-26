09719fa9ae50819f38aaf6ab11732ac8b16bda9b
krzysztofmakuch <55457951+krzysztofmakuch@users.noreply.github.com>
2020-10-26 13:29:52 +0100

    Add files via upload

---
+++ przyklad_forwhile.py
@@ -0,0 +1,20 @@
+L = ['a','d', 45, 89.23, 'asda']
+S = 'ala ma kota'
+a = 0
+'''
+while a < 7:
+    print(a)
+    a = a + 1
+'''
+'''
+for i in range(0,7):
+    print(i)
+'''
+
+for i in range(0,len(L)):
+    print(L[i])
+
+'''
+for letter in L:
+    print(letter)
+'''
