28f9becb1951ef9c329b40ab7817b0f936e2b77b
krzysztofmakuch <55457951+krzysztofmakuch@users.noreply.github.com>
2020-11-22 18:21:00 +0100

    Add files via upload

---
+++ kwargs_studenci.py
@@ -0,0 +1,16 @@
+def czyWypozyczone(**kwargs):
+    ''' Funkcja zwraca w postaci listy użytkowników (ID),
+    którzy wypożyczyli 'Potop'.
+    >>> czyWypozyczone(id1=['Potop', 'Pan Tadeusz'], id2=['Ferdydurke'])
+    [id1]
+    >>> czyWypozyczone(id1=['Potop', 'Pan Tadeusz'], id2=['Ferdydurke'], id3=['Zbrodnia i kara', 'Potop', 'Harry Potter'])
+    [id1, id3]
+    '''
+    listaId = []
+    #tu wchodzi kod:
+
+
+
+    return listaId
+
+
