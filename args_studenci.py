bbd2e38b9f3a1907734833c482f8fc9a00ee294c
krzysztofmakuch <55457951+krzysztofmakuch@users.noreply.github.com>
2020-11-22 17:59:24 +0100

    Add files via upload

---
+++ args_studenci.py
@@ -0,0 +1,19 @@
+def loginy(*args):
+    ''' program przyjmuje nieokreslona liczbe argumentow w postaci
+    stringa 'imie naziwsko' oddzielone spacja
+    >>> loginy('Marek Kowalski', 'Jan Gut', 'Anna Karenina', 'Yang Zgang')
+    ['MK13', 'JG6', 'AK12', 'YZ9']
+    >>> loginy('xya qwe', 'ASD as')
+    ['XQ6', 'AA5']
+
+    '''
+
+    #miejsce na kod:
+
+
+    
+
+    
+    return lista_loginow
+        
+        
